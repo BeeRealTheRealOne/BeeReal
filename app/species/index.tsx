96fd40b23f1466f883fbd92c9931ac0c6506f994
MitschlLUL <91023154+MitschlCoding@users.noreply.github.com>
2023-12-27 18:06:17 +0100

    [Task] added pagenation autofetch after end of list

--- app/species/index.tsx
+++ app/species/index.tsx
@@ -1,19 +1,51 @@
 import { useEffect, useState } from 'react';
 import pb from '../../constants/pocketbase';
-import SpeciesCard from '../../components/SpeciesCard';
 import { FlatList } from 'react-native-gesture-handler';
+import { View } from 'react-native';
+import { Link, Stack } from 'expo-router';
+import SpeciesItem from '../../components/SpeciesItem';
 
 function speciesList() {
     const [species, setSpecies] = useState<any>([]);
+
+    const [page, setPage] = useState(1);
+    const [maxPage, setMaxPage] = useState(1);
     useEffect(() => {
         pb.collection('species')
-            .getList(1, 50, { name: 'asc' })
+            .getList(page, 15)
             .then((res) => {
                 setSpecies(res.items);
+                setMaxPage(res.totalPages);
             })
             .catch((err) => console.error(err));
     }, []);
-    return <FlatList data={species} renderItem={({ item }) => <SpeciesCard key={item.id} name={item.name} description={item.description} scientificName={item.scientificName} categorie={item.categorie} imageURL={`http://192.168.2.109:80/api/files/species/${item.id}/${item.image}`}></SpeciesCard>} keyExtractor={(item: any) => item.id} />;
+
+    function loadMoreSpecies() {
+        if (page > maxPage) return;
+        pb.collection('species')
+            .getList(page + 1, 15)
+            .then((res) => {
+                setPage(page + 1);
+                setSpecies([...species, ...res.items]);
+            })
+            .catch((err) => console.error(err));
+    }
+    return (
+        <View>
+            <Stack.Screen options={{ title: 'Species' }} />
+            <FlatList
+                data={species}
+                renderItem={({ item }) => (
+                    <Link href={`/species/id/${item.id}/`} key={item.id}>
+                        <SpeciesItem key={item.id} name={item.name} scientificName={item.scientificName} imageURL={`http://192.168.2.109:80/api/files/species/${item.id}/${item.image}`}></SpeciesItem>
+                    </Link>
+                )}
+                keyExtractor={(item) => item.id}
+                onEndReached={loadMoreSpecies}
+                onEndReachedThreshold={0.5}
+            />
+        </View>
+    );
 }
 
 export default speciesList;
