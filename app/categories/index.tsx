f9d8b4b7e03c4cc8b689d9fa8548d521ddfa7e23
MitschlLUL <91023154+MitschlCoding@users.noreply.github.com>
2023-12-27 18:04:24 +0100

    [Fix] Added stack name for layout

--- app/categories/index.tsx
+++ app/categories/index.tsx
@@ -2,28 +2,32 @@ import { useEffect, useState } from 'react';
 import pb from '../../constants/pocketbase';
 import CategoriesItem from '../../components/CategoriesItem';
 import { FlatList } from 'react-native-gesture-handler';
-import { Link } from 'expo-router';
+import { Link, Stack } from 'expo-router';
+import { View } from 'react-native';
 
 function categoriesList() {
     const [species, setSpecies] = useState<any>([]);
     useEffect(() => {
         pb.collection('categories')
-            .getList(1, 50, { name: 'asc' })
+            .getList(1, 10, { name: 'asc' })
             .then((res) => {
                 setSpecies(res.items);
             })
             .catch((err) => console.error(err));
     }, []);
     return (
-        <FlatList
-            data={species}
-            renderItem={({ item }) => (
-                <Link href={`/categories/id/${item.id}/species/`} key={item.id}>
-                    <CategoriesItem id={item.id} key={item.id} name={item.name} scientificName={item.scientificName} />
-                </Link>
-            )}
-            keyExtractor={(item: any) => item.id}
-        />
+        <View>
+            <Stack.Screen options={{ title: 'Categories' }} />
+            <FlatList
+                data={species}
+                renderItem={({ item }) => (
+                    <Link href={`/categories/id/${item.id}/species/`} key={item.id}>
+                        <CategoriesItem id={item.id} key={item.id} name={item.name} scientificName={item.scientificName} />
+                    </Link>
+                )}
+                keyExtractor={(item: any) => item.id}
+            />
+        </View>
     );
 }
 
