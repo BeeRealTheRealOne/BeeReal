9f98086e5bbe8d1534e2c9ee2b3b1769ae23220e
MitschlLUL <91023154+MitschlCoding@users.noreply.github.com>
2024-01-02 13:43:41 +0100

    [Fix] fixed base color

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,12 +1,12 @@
 const Colors = {
-  base: "#ffffff",
-  baseLight: "#ffffff",
-  baseText: "#181307",
-  primary: "#f6af1a",
-  primaryText: "#171b27",
-  background: "#fcfaf5",
-  secondary: "#a2de8e",
-  accent: "#93d671",
+    base: '#f3f3f3',
+    baseLight: '#ffffff',
+    baseText: '#181307',
+    primary: '#f6af1a',
+    primaryText: '#171b27',
+    background: '#fcfaf5',
+    secondary: '#a2de8e',
+    accent: '#93d671',
 };
 
 export default Colors;
