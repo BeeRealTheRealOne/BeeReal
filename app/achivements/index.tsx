d5e15fcc0e8b59cdf68129f1927a29895dd3efa0
MitschlLUL <91023154+MitschlCoding@users.noreply.github.com>
2024-01-03 17:48:43 +0100

    [Task] achivement numSpecies numSightings

--- app/achivements/index.tsx
+++ app/achivements/index.tsx
@@ -1,11 +1,34 @@
-import { View, Text, StyleSheet } from 'react-native';
-import Colors from '../../constants/colors';
+import { useEffect, useState } from 'react';
+import pb from '../../constants/pocketbase';
+import { View, StyleSheet, Text } from 'react-native';
 import StyleLib from '../../constants/style';
+import Colors from '../../constants/colors';
 
 function achivementsView() {
+    const [numSightingsAchivements, setNumSightingsAchivements] = useState<number | null>();
+    const [numSpeciesAchivements, setNumSpeciesAchivements] = useState<number | null>();
+
+    useEffect(() => {
+        pb.collection('numSightingsAchivements')
+            .getOne(pb.authStore?.model?.id)
+            .then((res) => {
+                console.log(res);
+                setNumSightingsAchivements(res.cnt);
+            })
+            .catch((err) => console.error(err));
+        pb.collection('numSpeciesAchivements')
+            .getOne(pb.authStore?.model?.id)
+            .then((res) => {
+                console.log(res);
+                setNumSpeciesAchivements(res.cnt);
+            })
+            .catch((err) => console.error(err));
+    }, []);
+
     return (
-        <View style={[StyleLib.page]}>
-            <Text>Achivements</Text>
+        <View style={StyleLib.page}>
+            <Text>Number of Sightings: {numSightingsAchivements}</Text>
+            <Text>Number of Species Sighted: {numSpeciesAchivements}</Text>
         </View>
     );
 }
